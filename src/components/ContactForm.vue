50fa2fdd2a0bc96b5ad1b82ba7e857ee8ca7631d
Alan Maldonado <alan.maldonado@wizeline.com>
2019-04-01 15:37:04 -0600

    ✨ Grabing data from contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -7,14 +7,81 @@
 
       <form>
         <div class="text-fields">
-          <input type="text" class="text-input name-input" placeholder="Name">
-          <input type="text" class="text-input subject-input" placeholder="Subject">
-          <input type="email" class="text-input email-input" placeholder="Email Address">
-          <input type="text" class="text-input phone-input" placeholder="Phone Number">
-          <textarea class="text-input message-input" placeholder="Enter Message"></textarea>
+          <input type="text" class="text-input name-input" placeholder="Name" v-model="name">
+          <input
+            type="email"
+            class="text-input email-input"
+            placeholder="Email Address"
+            v-model="email"
+          >
+          <input
+            type="text"
+            class="text-input subject-input"
+            placeholder="Subject"
+            v-model="subject"
+          >
+
+          <input
+            type="text"
+            class="text-input phone-input"
+            placeholder="Phone Number"
+            v-model="phone"
+          >
+          <textarea class="text-input message-input" placeholder="Enter Message" v-model="message"></textarea>
         </div>
-        <button type="submit" class="btn-dark">Submit</button>
+        <button type="submit" class="btn-dark" @click.prevent="onSubmit">Submit</button>
       </form>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      phone: "",
+      name: "",
+      subject: "",
+      message: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(
+        this.email,
+        this.phone,
+        this.name,
+        this.subject,
+        this.message
+      );
+      if (!this.email || !this.message || !this.name || !this.subject) {
+        return;
+      }
+
+      const msg = {
+        to: "alan@maldonado.com",
+        from: this.email,
+        subject: `[maldonado.dev] ${this.subject}`,
+        text: `
+          ${this.message}
+
+          By ${this.name},
+          ${this.phone}
+          ${this.email}
+        `,
+        html: `
+          <div>
+            ${this.message}
+
+            <strong>
+              ${this.name}, ${this.phone}, ${this.email}
+            </strong>
+          </div>
+        `
+      };
+    }
+  }
+};
+</script>
+
