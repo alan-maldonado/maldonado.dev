642c15492bc6b7052698e4363da740d419c9c58c
Alan Maldonado <alan.maldonado@wizeline.com>
2019-04-01 13:33:42 -0600

    💄 Adding Footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header/>
     <Home/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 export default {
   name: "app",
   components: {
     Home,
-    Header
+    Header,
+    Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer id="main-footer">
+    <div class="footer-content container">
+      <p>Copyright &copy; 2019. All Rights Reserved</p>
+      <div class="social">
+        <a v-for="social in links" :href="social.href" :key="social.type" target="_blank">
+          <i class="fab" :class="`fa-${social.type}`"></i>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          type: "facebook",
+          href: "https://facebook.com/alan.maldonado"
+        },
+        {
+          type: "twitter",
+          href: "https://twitter.com/alan_maldonado"
+        },
+        {
+          type: "instagram",
+          href: "https://www.instagram.com/_alan.maldonado_/"
+        },
+        {
+          type: "linkedin",
+          href: "https://www.linkedin.com/in/alanmaldonado/"
+        }
+      ]
+    };
+  }
+};
+</script>
+
